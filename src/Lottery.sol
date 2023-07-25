7ec85529bf256a8e4ceb34a28b960b12771dbe60
cryptnology <jamie@cryptnology.dev>
2023-07-25 16:27:57 +0100

    Create fulfillRandomWords function

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -23,6 +23,9 @@ pragma solidity ^0.8.19;
 // internal & private view & pure functions
 // external & public view & pure functions
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 /**
  * @title Lottery Contract
  * @author Jamie Anderson
@@ -30,26 +33,60 @@ pragma solidity ^0.8.19;
  * @dev This contract implements Chainlink VRF to generate random numbers
  * and Chainlink automation to run the lottery
  */
-contract Lottery {
+contract Lottery is VRFConsumerBaseV2 {
     /**
      * Errors
      */
     error Lottery__NotEnoughEthSend();
+    error Lottery__TransferFailed();
+
+    /**
+     * Type declarations
+     */
+    enum LotteryState {
+        OPEN,
+        CALCULATING
+    }
 
     /**
      * State variables
      */
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
     uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
 
     address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+    address private s_recentWinner;
+    LotteryState private s_raffleState;
 
     /**
      * Events
      */
     event EnteredLottery(address indexed player);
+    event PickedWinner(address indexed winner);
 
-    constructor(uint256 entranceFee) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        s_lastTimeStamp = block.timestamp;
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterLottery() external payable {
@@ -60,7 +97,29 @@ contract Lottery {
         emit EnteredLottery(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() public {
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, i_subscriptionId, REQUEST_CONFIRMATIONS, i_callbackGasLimit, NUM_WORDS
+        );
+    }
+
+    function fulfillRandomWords(uint256, /* requestId */ uint256[] memory randomWords) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable winner = s_players[indexOfWinner];
+        s_recentWinner = winner;
+        s_raffleState = LotteryState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        emit PickedWinner(winner);
+        (bool success,) = winner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Lottery__TransferFailed();
+        }
+    }
 
     /**
      * External & public view & pure functions
