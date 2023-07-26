21857b1f20a54c57b8b40ae95be23e34e7cb0139
cryptnology <jamie@cryptnology.dev>
2023-07-26 09:43:01 +0100

    Create deploy script

--- script/DeployLottery.s.sol
+++ script/DeployLottery.s.sol
@@ -3,7 +3,33 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {Lottery} from "../src/Lottery.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployLottery is Script {
-    function run() external returns (Lottery) {}
+    function run() external returns (Lottery, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        (
+            uint256 entranceFee,
+            uint256 interval,
+            address vrfCoordinator,
+            bytes32 gasLane,
+            uint64 subscriptionId,
+            uint32 callbackGasLimit,
+            address link,
+            uint256 deployerKey
+        ) = helperConfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+        Lottery lottery = new Lottery(
+            entranceFee,
+            interval,
+            vrfCoordinator,
+            gasLane,
+            subscriptionId,
+            callbackGasLimit
+        );
+        vm.stopBroadcast();
+
+        return (lottery, helperConfig);
+    }
 }
